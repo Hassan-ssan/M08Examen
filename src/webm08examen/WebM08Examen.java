ca46b308bd389e5beff3cdafe156081557442fe3
Hassan <Hassan@DESKTOP-NM1PM8M>
2023-02-08 11:54:27 +0100

    Initialized

---
+++ src/webm08examen/WebM08Examen.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package webm08examen;
+
+/**
+ *
+ * @author Hassan
+ */
+public class WebM08Examen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
